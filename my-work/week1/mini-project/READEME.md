a06577bfc2e1ef108c3f640b716f8577f4d49afa
TheaSang <ys3421@nyu.edu>
2020-02-25 16:56:09 +0800

    mini-project

---
+++ my-work/week1/mini-project/READEME.md
@@ -0,0 +1,2 @@
+#my mini project#
+Thought this project, I have learned how to deal with data collected by Google Form. In this mini project, I have collected 41 data from my friends and visualize them. I tried to add some CSS style and JavaScript to make it looks not so boring. However, I still did not successfully to make achieve the function: when clicking the image, appearing the corresponding circles. 
---
+++ my-work/week1/mini-project/myScript.js
@@ -0,0 +1,497 @@
+console.log("hi");
+
+
+
+
+let data=[
+    {
+        "timestamp": "2020-02-19T07:39:49.152Z",
+        "yiDiandian": 6,
+        "coco": 8,
+        "cuoNeiVillage": 5,
+        "lelecha": 7,
+        "nayuki": 5,
+        "heytea": 10,
+        "koi": 5
+    },
+    {
+        "timestamp": "2020-02-19T07:39:55.501Z",
+        "yiDiandian": 1,
+        "coco": 3,
+        "cuoNeiVillage": 8,
+        "lelecha": 9,
+        "nayuki": 6,
+        "heytea": 6,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T07:40:03.905Z",
+        "yiDiandian": 7,
+        "coco": 5,
+        "cuoNeiVillage": 5,
+        "lelecha": 8,
+        "nayuki": 7,
+        "heytea": 10,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T07:41:20.605Z",
+        "yiDiandian": 7,
+        "coco": 8,
+        "cuoNeiVillage": 10,
+        "lelecha": 8,
+        "nayuki": 7,
+        "heytea": 10,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T07:42:55.432Z",
+        "yiDiandian": 8,
+        "coco": 8,
+        "cuoNeiVillage": 5,
+        "lelecha": 6,
+        "nayuki": 5,
+        "heytea": 8,
+        "koi": 5
+    },
+    {
+        "timestamp": "2020-02-19T07:43:59.131Z",
+        "yiDiandian": 7,
+        "coco": 7,
+        "cuoNeiVillage": 8,
+        "lelecha": 10,
+        "nayuki": 9,
+        "heytea": 10,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T07:44:49.061Z",
+        "yiDiandian": 7,
+        "coco": 6,
+        "cuoNeiVillage": 9,
+        "lelecha": 9,
+        "nayuki": 6,
+        "heytea": 9,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T07:45:50.464Z",
+        "yiDiandian": 9,
+        "coco": 7,
+        "cuoNeiVillage": 6,
+        "lelecha": 7,
+        "nayuki": 5,
+        "heytea": 10,
+        "koi": 7
+    },
+    {
+        "timestamp": "2020-02-19T07:47:03.619Z",
+        "yiDiandian": 8,
+        "coco": 5,
+        "cuoNeiVillage": 7,
+        "lelecha": 7,
+        "nayuki": 3,
+        "heytea": 10,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T07:47:18.599Z",
+        "yiDiandian": 7,
+        "coco": 6,
+        "cuoNeiVillage": 7,
+        "lelecha": 6,
+        "nayuki": 5,
+        "heytea": 8,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T07:49:31.084Z",
+        "yiDiandian": 8,
+        "coco": 4,
+        "cuoNeiVillage": 6,
+        "lelecha": 10,
+        "nayuki": 10,
+        "heytea": 10,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T07:50:51.090Z",
+        "yiDiandian": 9,
+        "coco": 8,
+        "cuoNeiVillage": 9,
+        "lelecha": 9,
+        "nayuki": 8,
+        "heytea": 10,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T07:51:26.916Z",
+        "yiDiandian": 4,
+        "coco": 0,
+        "cuoNeiVillage": 4,
+        "lelecha": 8,
+        "nayuki": 5,
+        "heytea": 7,
+        "koi": 7
+    },
+    {
+        "timestamp": "2020-02-19T07:52:13.753Z",
+        "yiDiandian": 10,
+        "coco": 8,
+        "cuoNeiVillage": 8,
+        "lelecha": 8,
+        "nayuki": 8,
+        "heytea": 6,
+        "koi": 7
+    },
+    {
+        "timestamp": "2020-02-19T07:55:04.432Z",
+        "yiDiandian": 8,
+        "coco": 6,
+        "cuoNeiVillage": 7,
+        "lelecha": 8,
+        "nayuki": 7,
+        "heytea": 9,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T07:57:29.142Z",
+        "yiDiandian": 8,
+        "coco": 7,
+        "cuoNeiVillage": 5,
+        "lelecha": 6,
+        "nayuki": 6,
+        "heytea": 6,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T07:59:19.513Z",
+        "yiDiandian": 5,
+        "coco": 4,
+        "cuoNeiVillage": 7,
+        "lelecha": 8,
+        "nayuki": 7,
+        "heytea": 9,
+        "koi": 7
+    },
+    {
+        "timestamp": "2020-02-19T08:08:36.615Z",
+        "yiDiandian": 10,
+        "coco": 8,
+        "cuoNeiVillage": 6,
+        "lelecha": 3,
+        "nayuki": 3,
+        "heytea": 5,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T08:21:34.262Z",
+        "yiDiandian": 9,
+        "coco": 8,
+        "cuoNeiVillage": 10,
+        "lelecha": 10,
+        "nayuki": 10,
+        "heytea": 10,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T08:23:14.370Z",
+        "yiDiandian": 9,
+        "coco": 7,
+        "cuoNeiVillage": 5,
+        "lelecha": 8,
+        "nayuki": 4,
+        "heytea": 6,
+        "koi": 6
+    },
+    {
+        "timestamp": "2020-02-19T08:29:45.057Z",
+        "yiDiandian": 7,
+        "coco": 7,
+        "cuoNeiVillage": 8,
+        "lelecha": 5,
+        "nayuki": 5,
+        "heytea": 2,
+        "koi": 3
+    },
+    {
+        "timestamp": "2020-02-19T08:32:41.185Z",
+        "yiDiandian": 10,
+        "coco": 2,
+        "cuoNeiVillage": 10,
+        "lelecha": 8,
+        "nayuki": 5,
+        "heytea": 5,
+        "koi": 5
+    },
+    {
+        "timestamp": "2020-02-19T08:33:30.290Z",
+        "yiDiandian": 4,
+        "coco": 8,
+        "cuoNeiVillage": 9,
+        "lelecha": 8,
+        "nayuki": 8,
+        "heytea": 8,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T09:00:53.298Z",
+        "yiDiandian": 7,
+        "coco": 7,
+        "cuoNeiVillage": 6,
+        "lelecha": 8,
+        "nayuki": 8,
+        "heytea": 9,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T09:04:38.282Z",
+        "yiDiandian": 4,
+        "coco": 3,
+        "cuoNeiVillage": 4,
+        "lelecha": 6,
+        "nayuki": 7,
+        "heytea": 8,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T09:56:49.232Z",
+        "yiDiandian": 7,
+        "coco": 10,
+        "cuoNeiVillage": 10,
+        "lelecha": 9,
+        "nayuki": 8,
+        "heytea": 4,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T09:57:20.144Z",
+        "yiDiandian": 6,
+        "coco": 6,
+        "cuoNeiVillage": 10,
+        "lelecha": 8,
+        "nayuki": 5,
+        "heytea": 8,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T10:15:34.294Z",
+        "yiDiandian": 0,
+        "coco": 0,
+        "cuoNeiVillage": 0,
+        "lelecha": 0,
+        "nayuki": 0,
+        "heytea": 0,
+        "koi": 0
+    },
+    {
+        "timestamp": "2020-02-19T10:26:20.525Z",
+        "yiDiandian": 8,
+        "coco": 7,
+        "cuoNeiVillage": 7,
+        "lelecha": 8,
+        "nayuki": 6,
+        "heytea": 6,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T10:28:27.514Z",
+        "yiDiandian": 8,
+        "coco": 5,
+        "cuoNeiVillage": 6,
+        "lelecha": 8,
+        "nayuki": 6,
+        "heytea": 10,
+        "koi": 7
+    },
+    {
+        "timestamp": "2020-02-19T10:56:28.086Z",
+        "yiDiandian": 9,
+        "coco": 7,
+        "cuoNeiVillage": 9,
+        "lelecha": 9,
+        "nayuki": 8,
+        "heytea": 8,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T11:02:54.745Z",
+        "yiDiandian": 2,
+        "coco": 8,
+        "cuoNeiVillage": 9,
+        "lelecha": 10,
+        "nayuki": 8,
+        "heytea": 7,
+        "koi": 4
+    },
+    {
+        "timestamp": "2020-02-19T11:41:53.435Z",
+        "yiDiandian": 3,
+        "coco": 1,
+        "cuoNeiVillage": 3,
+        "lelecha": 2,
+        "nayuki": 0,
+        "heytea": 0,
+        "koi": 1
+    },
+    {
+        "timestamp": "2020-02-19T12:07:32.696Z",
+        "yiDiandian": 9,
+        "coco": 10,
+        "cuoNeiVillage": 7,
+        "lelecha": 6,
+        "nayuki": 7,
+        "heytea": 4,
+        "koi": 2
+    },
+    {
+        "timestamp": "2020-02-19T12:43:18.858Z",
+        "yiDiandian": 8,
+        "coco": 5,
+        "cuoNeiVillage": 6,
+        "lelecha": 10,
+        "nayuki": 8,
+        "heytea": 10,
+        "koi": 5
+    },
+    {
+        "timestamp": "2020-02-19T15:19:03.602Z",
+        "yiDiandian": 6,
+        "coco": 5,
+        "cuoNeiVillage": 10,
+        "lelecha": 6,
+        "nayuki": 9,
+        "heytea": 8,
+        "koi": 3
+    },
+    {
+        "timestamp": "2020-02-19T15:33:44.418Z",
+        "yiDiandian": 6,
+        "coco": 6,
+        "cuoNeiVillage": 6,
+        "lelecha": 8,
+        "nayuki": 8,
+        "heytea": 9,
+        "koi": 9
+    },
+    {
+        "timestamp": "2020-02-19T15:44:12.359Z",
+        "yiDiandian": 6,
+        "coco": 6,
+        "cuoNeiVillage": 9,
+        "lelecha": 10,
+        "nayuki": 9,
+        "heytea": 10,
+        "koi": 8
+    },
+    {
+        "timestamp": "2020-02-19T15:46:18.980Z",
+        "yiDiandian": 7,
+        "coco": 8,
+        "cuoNeiVillage": 9,
+        "lelecha": 9,
+        "nayuki": 7,
+        "heytea": 10,
+        "koi": 10
+    },
+    {
+        "timestamp": "2020-02-19T15:47:03.020Z",
+        "yiDiandian": 8,
+        "coco": 8,
+        "cuoNeiVillage": 8,
+        "lelecha": 10,
+        "nayuki": 8,
+        "heytea": 10,
+        "koi": 6
+    },
+    {
+        "timestamp": "2020-02-19T16:01:18.219Z",
+        "yiDiandian": 7,
+        "coco": 7,
+        "cuoNeiVillage": 9,
+        "lelecha": 8,
+        "nayuki": 7,
+        "heytea": 9,
+        "koi": 8
+    }
+]
+
+function averageData(data){
+
+  let newData = [];
+  let keys = Object.keys(data[0]);
+  for(let i = 0; i < keys.length; i++){
+
+    let key = keys[i];
+
+    let sum = 0;
+    let num = 0;
+    for(let j = 0; j < data.length; j++){
+      let datum = data[j];
+      if(key in datum){
+      sum += datum[key];
+        num++;
+      }
+    }
+
+    let avg = sum/num;
+    if(!isNaN(avg)){
+      let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
+      newData.push(newDataPoint);
+    }
+  }
+  return newData;
+}
+function drawBar(){
+  for(let i =0;i <transformedData.length;i++){
+  drawBar(i);
+
+}
+
+}
+function buttonClick(){
+let colors = ["#f1b8e4", "#f1f1b8", "#f1ccb8","#F5DAE6","#b8f1ed","#DCFF93","#FFE543"];
+let circleleft=["20px","200px","400px","600px","820px","1000px","1200px"]
+
+let transformedData =averageData(data);
+console.log(transformedData);
+console.log(data);
+for(let i =0;i <transformedData.length;i++){
+  let datapoint = transformedData[i];
+  let newAverage = datapoint.average.toFixed(2)
+  console.log(newAverage);
+  console.log(datapoint.name);
+  // var bar = document.createElement("span");
+  let bar = document.createElement("span");
+  // bar.className ="bar";
+  bar.innerHTML = datapoint.name + ":"+"<br>"+ newAverage ;
+  bar.style.textAlign = "center";
+  bar.style.color = "#e1622f";
+
+  bar.style.fontSize = "25px";
+  bar.style.fontFamily="myFirstFont";
+  bar.style.height = datapoint.average*40 +"px";
+  bar.style.width = datapoint.average*40 +"px";
+bar.style.backgroundColor= colors[i];
+bar.style.left=circleleft[i];
+//
+  document.getElementById('viz').appendChild(bar);
+}
+  // var finalpop = document.getElementById('show').appendChild(pop);
+  // var finalpop =document.getElementById('show').appendChild(pop).innerHTML;
+  //   document.getElementById('show').innerHTML= finalpop;
+  // document.getElementById('show').appendChild(pop)
+
+
+
+
+//   let bar = document.createElement("span");
+//   // bar.className ="bar";
+//   bar.innerHTML = datapoint.name;
+//   bar.style.height = datapoint.average*40 +"px";
+//   bar.style.width = datapoint.average*40 +"px";
+// bar.style.backgroundColor= colors[i];
+// bar.style.left=circleleft[i];
+//
+//   document.getElementById('viz').appendChild(bar);
+}
