29494d7a0bd42cc61553c10a7904c2865c38b46e
TheaSang <ys3421@nyu.edu>
2020-04-09 17:22:37 +0800

    exercise

---
+++ my-work/week9/code exercise/line-1-start/script.js
@@ -0,0 +1,47 @@
+let w = 900;
+let h = 500;
+
+let viz = d3.select("#container")
+  .append("svg")
+    .style("width", w)
+    .style("height", h)
+    .style("outline", "solid black")
+;
+
+let data = [
+  [
+    [0,   100],
+    [300, 400],
+    [600, 300],
+    [900, 350]
+  ],
+  [
+    [0,   300],
+    [230, 350],
+    [900, 90 ]
+  ]
+];
+
+let lineMaker=d3.line();
+
+// function lineFunction(d,i){
+//   let dataString = "";
+//   for (let i=0;i<d.length;i++){
+//     if(i==0){
+//       dataString +="M";
+//     }else{
+//     dataString +="L";
+//   }
+//     dataString +=d[i].join(" ")
+//   }
+//   return dataString;
+// }
+
+let graphGroup = viz.append("g").attr("class", "graphGroup");
+graphGroup.selectAll(".line").data(data).enter()
+.append("path")
+.attr("d",lineMaker)
+.attr("fill","none")
+.attr("stroke","black")
+.attr("stroke-width","5")
+;
